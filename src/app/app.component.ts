471ee5329841d0b02286f432a56c36d799ed08c3
Victorvs1967 <victorsmirnov@me.com>
2023-04-05 10:37:50 +0300

    create angular social login app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { Component, OnInit, inject } from '@angular/core';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'angular-social-login';
+export class AppComponent implements OnInit {
+
+  private auth = inject(SocialAuthService);
+
+  user?: SocialUser;
+  loggedIn?: boolean;
+
+  ngOnInit(): void {
+    this.auth.authState
+      .pipe(map(user => {
+        this.user = user;
+        this.loggedIn = (user != null);
+        console.log(user);
+      }))
+      .subscribe()
+  }
+
+  signOut(): void {
+    this.auth.signOut();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleLoginProvider, GoogleSigninButtonModule, SocialAuthServiceConfig, SocialLoginModule } from '@abacritt/angularx-social-login';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,29 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule,
+    GoogleSigninButtonModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '693108646005-8tmp2ciftj7jlalomjvrljq9hb4titb8.apps.googleusercontent.com'
+            )
+          },
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
